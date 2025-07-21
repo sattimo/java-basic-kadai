4b132078025ccc09ca76407fa2c783ee3291357b
sattimo <h.teranishi@fukuya-k.co.jp>
2025-07-21 16:44:21 +0900

    コード書き換え

--- kadai_018/KatoTaro_Chapter18.java
+++ kadai_018/KatoTaro_Chapter18.java
@@ -2,16 +2,13 @@ package kadai_018;
 
 public class KatoTaro_Chapter18 extends Kato_Chapter18{
 	
-	public KatoTaro_Chapter18() {
-		
-		super("太郎");
-				
+	public void setGivenName () {
+		givenName = "太郎";
 	}
 	
-	@Override
-	
-	public void eachIntroduce() {
+	public void eachIntroduce () {
 		System.out.println("Javaが得意です");
 	}
-
 }
+
+
